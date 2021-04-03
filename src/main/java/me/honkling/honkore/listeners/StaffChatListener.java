a1d7bf42eee9f48b51d0d303c504f23cbc3ac34c
Shay Punter <shay@shaypunter.co.uk>
2021-04-03 02:59:53 +0100

    Refactored unused class

--- src/main/java/me/honkling/honkore/listeners/StaffChatListener.java
+++ src/main/java/me/honkling/honkore/listeners/StaffChatListener.java
@@ -1,30 +1,27 @@
 package me.honkling.honkore.listeners;
 
-import me.honkling.honkore.Honkore;
 import me.honkling.honkore.lib.Utils;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+/*
+	ISSUE: UNUSED CLASS
+ */
 public class StaffChatListener implements Listener {
-
-	private Honkore plugin;
-
-	public StaffChatListener(Honkore plugin) {
-		this.plugin = plugin;
-	}
 
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onChatMessage(AsyncPlayerChatEvent e) {
-		FileConfiguration config = plugin.getConfig();
 		Player p = e.getPlayer();
 		String msg = e.getMessage();
-		if(p.hasMetadata("staffchat") && p.getMetadata("staffchat").get(0).asBoolean()) {
-			e.setCancelled(true);
-			Utils.staffChat(p, msg);
+
+		if (p.hasMetadata("staffchat")) {
+			if (p.getMetadata("staffchat").get(0).asBoolean()) {
+				e.setCancelled(true);
+				Utils.staffChat(p, msg);
+			}
 		}
 	}
 
