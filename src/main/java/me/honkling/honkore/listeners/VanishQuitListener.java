e96d013c76575e2bf038116b0a094fe75c3eec8d
Shay Punter <shay@shaypunter.co.uk>
2021-04-03 02:56:10 +0100

    Refactored the VanishQuitListener class

--- src/main/java/me/honkling/honkore/listeners/VanishQuitListener.java
+++ src/main/java/me/honkling/honkore/listeners/VanishQuitListener.java
@@ -1,5 +1,6 @@
 package me.honkling.honkore.listeners;
 
+import me.honkling.honkore.Honkore;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,12 +10,13 @@ import org.bukkit.plugin.Plugin;
 
 public class VanishQuitListener implements Listener {
 
+	private final Honkore plugin = Honkore.getInstance();
+
 	@EventHandler
 	public void onPlayerQuit(final PlayerQuitEvent e) {
 		Player p = e.getPlayer();
-		Plugin plugin = Bukkit.getPluginManager().getPlugin("honkore");
-		assert plugin != null;
 		p.removeMetadata("vanish", plugin);
+
 		for(Player member : Bukkit.getOnlinePlayers()) {
 			p.showPlayer(plugin, member);
 			member.showPlayer(plugin, p);
